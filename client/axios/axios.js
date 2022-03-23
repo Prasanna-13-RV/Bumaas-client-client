bdd9130e4fdd1efba46e0d5e8c1eb9c5ce3ccac3
Lohith <srklohith05@gmail.com>
2022-03-23 21:23:22 +0530

    Loki 9:23

--- client/axios/axios.js
+++ client/axios/axios.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
-export const projectCreate = (project) => {
-    axios.post('http://localhost:8090/forecast', project)
-}
+export const projectCreate = async (project) =>
+	await axios.post('http://192.168.0.103:8090/forecast', project);
+
+export const projectDelete = async (id) =>
+	await axios.delete(`http://192.168.0.103:8090/forecast/${id}`);
