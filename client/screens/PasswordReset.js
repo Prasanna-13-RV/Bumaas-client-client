1724840655fc9875ec64cf1099d1cbd562af2ce8
Prasanna <prasannavelmurugan0200@gmail.com>
2022-03-26 12:52:19 +0530

    rv

--- client/screens/PasswordReset.js
+++ client/screens/PasswordReset.js
@@ -1,46 +1,75 @@
-import { View, Text,StyleSheet,TextInput,TouchableOpacity } from 'react-native'
-import React ,{useState} from 'react'
-import { auth } from "../firebase/firebase";
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    TouchableOpacity,
+} from "react-native";
+import React, {useState} from "react";
+import {auth} from "../firebase/firebase";
+
 const PasswordReset = () => {
     const [email, setEmail] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
     const handleReset = () => {
         auth.sendPasswordResetEmail(email.trim())
-      .then(() => {
-        setErrorMessage("Password reset email sent");
-        // Password reset email sent!
-        // ..
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-        console.log(error);
-        setErrorMessage(errorMessage);
-      });
-      };
-  return (
-    <View style={styles.container}>
-      <Text>PasswordReset</Text>
-      <TextInput
-        placeholder="Enter your password"
-        style={styles.input}
-        onChangeText={(text) => setEmail(text)}
-        value={email}
-      />
-      <Text>{errorMessage}</Text>
-       <TouchableOpacity style={styles.button} onPress={handleReset}>
-        <Text style={styles.button}>Submit</Text>
-      </TouchableOpacity>
-    </View>
-  )
-}
+            .then(() => {
+                setErrorMessage("Password reset email sent");
+                // Password reset email sent!
+                // ..
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ..
+                console.log(error);
+                setErrorMessage(errorMessage);
+            });
+    };
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Password Reset</Text>
+            <TextInput
+                placeholder="Enter your password"
+                style={styles.input}
+                onChangeText={(text) => setEmail(text)}
+                value={email}
+            />
+            <Text>{errorMessage}</Text>
+            <TouchableOpacity style={styles.button} onPress={handleReset}>
+                <Text style={styles.button}>Submit</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+export default PasswordReset;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "center",
-      }
-})
-export default PasswordReset
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: "black",
+        width: "80%",
+        marginBottom: 10,
+        padding: 10,
+    },
+    title: {
+        fontSize: 20,
+        marginBottom: 10,
+    },
+    button: {
+        backgroundColor: "blue",
+        padding: 5,
+        paddingLeft: 10,
+        paddingRight: 10,
+        alignContent: "center",
+        justifyContent: "center",
+        fontSize: 15,
+        color: "white",
+        margin: 2,
+    },
+});
