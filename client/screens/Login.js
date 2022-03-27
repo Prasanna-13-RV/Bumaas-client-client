a0ab857f9707dff4147683e94fad3da6b991fe17
Prasanna <prasannavelmurugan0200@gmail.com>
2022-03-27 17:59:23 +0530

    rv

--- client/screens/Login.js
+++ client/screens/Login.js
@@ -1,32 +1,29 @@
 import {
-	View,
-	Text,
-	StyleSheet,
-	TextInput,
-	TouchableOpacity
-} from 'react-native';
-import React, { useState } from 'react';
-import { auth } from '../firebase/firebase';
-const Password = ({ navigation }) => {
-	const [password, setPassword] = useState('');
-	const [email, setEmail] = useState('');
-	const [errorMessage, setErrorMessage] = useState('');
-	const handleLogin = () => {
-
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    TouchableOpacity,
+} from "react-native";
+import React, {useState} from "react";
+import {auth} from "../firebase/firebase";
+const Password = ({navigation}) => {
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const handleLogin = async () => {
         await getCustomerWithMail(email).then((res) => {
-			if (res[0] === undefined) {
-				setErrorMessage('Email not found');
-			} else {
-				
-                auth
-                    .signInWithEmailAndPassword(email.trim(), password)
+            if (res[0] === undefined) {
+                setErrorMessage("Email not found");
+            } else {
+                auth.signInWithEmailAndPassword(email.trim(), password)
                     .then((userCredential) => {
                         const user = userCredential.user;
                         console.log(user);
                         console.log(user.displayName);
-                        navigation.push('ProfileScreen', {
-							customer_id: res[0].customer_id
-						});        
+                        navigation.push("ProfileScreen", {
+                            customer_id: res[0].customer_id,
+                        });
                     })
                     .catch((error) => {
                         const errorCode = error.code;
@@ -34,83 +31,85 @@ const Password = ({ navigation }) => {
                         const errorMessage = error.message;
                         setErrorMessage(errorMessage);
                     });
-			}
-		});
-	};
+            }
+        });
+    };
 
-	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>Login</Text>
-			<TextInput
-				placeholder='Enter your ID'
-				style={styles.input}
-				onChangeText={(text) => setEmail(text)}
-				value={email}
-			/>
-			<TextInput
-				placeholder='Enter your password'
-				style={styles.input}
-				onChangeText={(text) => setPassword(text)}
-				value={password}
-			/>
-			<Text>{errorMessage}</Text>
-			<View style={styles.allbutton}>
-				<TouchableOpacity style={styles.button} onPress={handleLogin}>
-					<Text style={styles.button}>Login</Text>
-				</TouchableOpacity>
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Login</Text>
+            <TextInput
+                placeholder="Enter your ID"
+                style={styles.input}
+                onChangeText={(text) => setEmail(text)}
+                value={email}
+                keyboardType="email-address"
+            />
+            <TextInput
+                placeholder="Enter your password"
+                style={styles.input}
+                onChangeText={(text) => setPassword(text)}
+                secureTextEntry={true}
+                value={password}
+            />
+            <Text>{errorMessage}</Text>
+            <View style={styles.allbutton}>
+                <TouchableOpacity style={styles.button} onPress={handleLogin}>
+                    <Text style={styles.button}>Login</Text>
+                </TouchableOpacity>
 
-				<TouchableOpacity
-					style={styles.button}
-					onPress={() => navigation.push('LoginScreen')}
-				>
-					<Text style={styles.button}>Sign up</Text>
-				</TouchableOpacity>
-				<TouchableOpacity
-					style={styles.button}
-					onPress={() => navigation.push('PasswordReset')}
-				>
-					<Text style={styles.button}>Reset Password?</Text>
-				</TouchableOpacity>
-			</View>
-		</View>
-	);
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => navigation.push("LoginScreen")}
+                >
+                    <Text style={styles.button}>Sign up</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => navigation.push("PasswordReset")}
+                >
+                    <Text style={styles.button}>Reset Password?</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center'
-	},
-	input: {
-		borderWidth: 1,
-		borderColor: 'black',
-		width: '80%',
-		marginBottom: 10,
-		padding: 10
-	},
-	title: {
-		fontSize: 30,
-		marginBottom: 10
-	},
-	allbutton: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignContent: 'center',
-		width: '80%'
-	},
-	button: {
-		backgroundColor: 'blue',
-		padding: 5,
-		margin: 2,
-		// borderRadius: 10,
-		color: 'white'
-	},
-	text: {
-		marginTop: 10,
-		color: 'red'
-	}
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: "black",
+        width: "80%",
+        marginBottom: 10,
+        padding: 10,
+    },
+    title: {
+        fontSize: 30,
+        marginBottom: 10,
+    },
+    allbutton: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignContent: "center",
+        width: "80%",
+    },
+    button: {
+        backgroundColor: "blue",
+        padding: 5,
+        margin: 2,
+        // borderRadius: 10,
+        color: "white",
+    },
+    text: {
+        marginTop: 10,
+        color: "red",
+    },
 });
 
 export default Password;
