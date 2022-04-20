658040c2ee9f589f73b9b3e30dcafc45fe8be1f7
Lohith <srklohith05@gmail.com>
2022-04-20 20:56:23 +0530

    changes

--- axios/axios.js
+++ axios/axios.js
@@ -1,41 +1,37 @@
 import axios from 'axios';
 
+const baseUrl = 'http://192.168.0.104:8090';
+
 export const projectGet = async (customer_id) => {
-	return await axios
-		.get(`http://192.168.0.103:8090/getproject/${customer_id}`)
-		.then((res) => {
-			console.log(res);
-			return res.data;
-		});
+	return await axios.get(`${baseUrl}/getproject/${customer_id}`).then((res) => {
+		console.log(res);
+		return res.data;
+	});
 };
 export const forecastGet = async (customer_id) => {
 	return await axios
-		.get(`http://192.168.0.103:8090/getforecast/${customer_id}`)
+		.get(`${baseUrl}/getforecast/${customer_id}`)
 		.then((res) => {
 			console.log(res);
 			return res.data;
 		});
 };
 export const projectGetSingle = async (forecastid) => {
-	console.log(forecastid,'llll');
-	return await axios
-		.get(`http://192.168.0.103:8090/getfore/${forecastid}`)
-		.then((res) => {
-			console.log(res);
-			return res.data;
-		});
+	console.log(forecastid, 'llll');
+	return await axios.get(`${baseUrl}/getfore/${forecastid}`).then((res) => {
+		console.log(res);
+		return res.data;
+	});
 };
 
 export const getCustomerWithMail = async (email) => {
-	return await axios
-		.get(`http://192.168.0.103:8090/getcustomer/${email}`)
-		.then((res) => {
-			console.log(res);
-			return res.data;
-		});
+	return await axios.get(`${baseUrl}/getcustomer/${email}`).then((res) => {
+		console.log(res);
+		return res.data;
+	});
 };
-export const projectCreate = async (values,customer_id) =>
-	await axios.post(`http://192.168.0.103:8090/forecast/${customer_id}`, values);
+export const projectCreate = async (values, customer_id) =>
+	await axios.post(`${baseUrl}/forecast/${customer_id}`, values);
 
 export const projectDelete = async (id) =>
-	await axios.delete(`http://192.168.0.103:8090/forecast/${id}`);
+	await axios.delete(`${baseUrl}/forecast/${id}`);
